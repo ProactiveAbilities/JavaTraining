30a625a53c2b6d5c9bb188f436e2aff54cd32a6f
nsa.ext <nsa.ext@HP-Navision.extenda.es>
2015-11-19 14:10:56 +0100

    Second commit

--- src/main/java/com/northwind/repository/BlogRepository.java
+++ src/main/java/com/northwind/repository/BlogRepository.java
@@ -1,9 +1,12 @@
 package com.northwind.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.northwind.entity.Blog;
+import com.northwind.entity.User;
 
 public interface BlogRepository extends JpaRepository<Blog, Integer>{
 
-	
+	List<Blog> findByUser(User user);
 }
--- src/main/java/com/northwind/repository/ItemRepository.java
+++ src/main/java/com/northwind/repository/ItemRepository.java
@@ -1,9 +1,14 @@
 package com.northwind.repository;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.northwind.entity.Blog;
 import com.northwind.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Integer>{
 
+	List<Item> findByBlog(Blog blog, Pageable pageable);
 	
 }
